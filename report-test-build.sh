e141a6a9f47dc455f844164442564b048a9166e9
App Generator <51070104+app-generator@users.noreply.github.com>
2022-06-26 22:28:46 +0300

    dummy

--- report-test-build.sh
+++ report-test-build.sh
@@ -97,7 +97,7 @@ readarray -t repoArrays < <(jq -c '.repositories[]' repositories.json) # Reads t
             echo "Serving application with $command"
             chromium-browser --headless --screenshot=$sshot_name "http://localhost:3000"
 
-            echo 'GH report OK (body)' | mail chirilovadrian@gmail.com –s 'React Report'
+            echo "test body" | mail -s 'test subject' chirilovadrian@gmail.com 
 
         else
             print_message "error" "$repoDir Serving build failed $command"
