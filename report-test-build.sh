4745ad2b2b6fb94866ef1716ec647c7db0574d07
App Generator <51070104+app-generator@users.noreply.github.com>
2022-06-27 10:13:23 +0300

    Berry / Ubuntu / Yarn & NPM / Node 10.x -> 18.x

--- report-test-build.sh
+++ report-test-build.sh
@@ -37,7 +37,7 @@ save_report() {
 
 NODE_VERSION=$(node --version)
 
-declare -a NODE_COMMANDS=("npm") # Default ("yarn" "npm") 
+declare -a NODE_COMMANDS=("npm" "yarn")
 
 echo "Starting compatibily test"
 
