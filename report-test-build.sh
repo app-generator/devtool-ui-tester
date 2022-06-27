1d8da1a9bfd575daf6a037d90ef8aed6fc23ce60
App Generator <51070104+app-generator@users.noreply.github.com>
2022-06-27 11:51:11 +0300

    dummy

--- report-test-build.sh
+++ report-test-build.sh
@@ -53,97 +53,99 @@ for repo in "${repoArrays[@]}"; do
       
         report_file="${repoDir}-${command}-node-${NODE_VERSION}-STATUS_NA.log"
         report_sshot="${repoDir}-${command}-node-${NODE_VERSION}-sshot.png"
+        report_BASE="./reports"
 
         # File creation    
-        echo ' ' > ./reports/$report_file
+        echo ' ' > $report_BASE/$report_file
 
-        echo "START TESTS for ${repoDir} / ${command} / NodeJS-${NODE_VERSION}" >> ./reports/$report_file 
-        echo ' '                                                                >> ./reports/$report_file
+        echo "START TESTS for ${repoDir} / ${command} / NodeJS-${NODE_VERSION}" >> $report_BASE/$report_file 
+        echo ' '                                                                >> $report_BASE/$report_file
 
         # Force removal for each compiler
-        echo "  > Force REMOVAL $repoURL" >> ./reports/$report_file
-        rm -rf $repoURL
-        echo "   ...ok" >> ./reports/$report_file
+        echo "  > Force REMOVAL $repoDir"    >> $report_BASE/$report_file
+        rm -rf $repoDir
+        echo "   ...ok"                      >> $report_BASE/$report_file
 
-        echo "  > Cloning $repoURL" >> ./reports/$report_file   
+        echo "  > Cloning $repoURL"          >> $report_BASE/$report_file   
         if git clone $repoURL; then
-            echo "   ...ok" >> ./reports/$report_file
+            echo "   ...ok"                  >> $report_BASE/$report_file
         else
-            echo "   ...err" >> ./reports/$report_file
-            save_report ./reports/$report_file ERR
+            echo "   ...err"                 >> $report_BASE/$report_file
+            save_report $report_BASE/$report_file ERR
             exit 1
         fi
 
         # !!! NEW Directory !!! 
         cd $(basename $repoURL .git)
+        report_BASE="../reports"
 
-        echo " > [$command] Install modules " >> ../reports/$report_file
+        echo " > [$command] Install modules " >> $report_BASE/$report_file
         if $command install; then
-            echo "   ...ok"  >> ../reports/$report_file
+            echo "   ...ok"                   >> $report_BASE/$report_file
         else
-            echo "   ...err" >> ../reports/$report_file
-            save_report ../reports/$report_file ERR
+            echo "   ...err"                  >> $report_BASE/$report_file
+            save_report $report_BASE/$report_file ERR
             continue
         fi
       
-        echo " > [$command] Running tests " >> ../reports/$report_file
+        echo " > [$command] Running tests "   >> $report_BASE/$report_file
         if CI=true $command test --passWithNoTests; then
-            echo "   ...ok"  >> ../reports/$report_file
+            echo "   ...ok"                   >> $report_BASE/$report_file
         else
-            echo "   ...err" >> ../reports/$report_file
-            save_report ../reports/$report_file ERR           
+            echo "   ...err"                  >> $report_BASE/$report_file
+            save_report $report_BASE/$report_file ERR           
             continue
         fi
 
-        echo " > [$command] Compile Sources " >> $report_file
+        echo " > [$command] Compile Sources " >> $report_BASE/$report_file
         if [ "$command" = "npm" ]; then
             if $command run build; then
-                echo "   ...ok"  >> ../reports/$report_file
+                echo "   ...ok"               >> $report_BASE/$report_file
             else
-                echo "   ...err" >> ../reports/$report_file
-                save_report ../reports/$report_file ERR
+                echo "   ...err"              >> $report_BASE/$report_file
+                save_report $report_BASE/$report_file ERR
                 continue
             fi
         else 
             if $command build; then
-                echo "   ...ok"  >> ../reports/$report_file
+                echo "   ...ok"                >> $report_BASE/$report_file
             else
-                echo "   ...err" >> ../reports/$report_file
-                save_report ../reports/$report_file ERR
+                echo "   ...err"               >> $report_BASE/$report_file
+                save_report $report_BASE/$report_file ERR
                 continue
             fi
         fi
 
-        echo " > Install `serve` utility " >> $report_file
+        echo " > Install `serve` utility "     >> $report_BASE/$report_file
         if [ "$command" = "npm" ]; then
             npm i -g serve
-            echo "   ...ok (via NPM)"  >> ../reports/$report_file
+            echo "   ...ok (via NPM)"          >> $report_BASE/$report_file
         else
             yarn global add serve
-            echo "   ...ok (via YARN)" >> ../reports/$report_file
+            echo "   ...ok (via YARN)"         >> $report_BASE/$report_file
         fi
 
-        echo " > [$command] Starting APP in browser" >> $report_file 
+        echo " > [$command] Starting APP in browser" >> $report_BASE/$report_file
         if serve -s build & 
         then
-            echo "   ...ok" >> ../reports/$report_file
+            echo "   ...ok" >> $report_BASE/$report_file
 
-            echo " > Saving SSHot -> $report_sshot" >> ../reports/$report_file
+            echo " > Saving SSHot -> $report_sshot" >> $report_BASE/$report_file
 
             chromium-browser --headless --screenshot=$report_sshot "http://localhost:3000"
             mv $report_sshot ../reports/
 
-            echo "   ...ok" >> ../reports/$report_file
+            echo "   ...ok" >> $report_BASE/$report_file
 
         else
-            echo "   ...err" >> ../reports/$report_file
-            save_report ../reports/$report_file ERR
+            echo "   ...err" >> $report_BASE/$report_file
+            save_report $report_BASE/$report_file ERR
             continue
         fi
 
         killall -9 node
 
-        save_report $report_file OK    
+        save_report $report_BASE/$report_file OK    
 
         # Iterate on the next REPO    
         cd ..    
